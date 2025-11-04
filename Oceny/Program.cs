b93ab513c35f246df214dbf6904d64f1c94df7eb
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-11-04 13:53:37 +0100

    Oceny_update

--- Oceny/Program.cs
+++ Oceny/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wprowadź ocenę: ");
+            string ocena_string = Console.ReadLine();
+            int ocena = int.Parse(ocena_string);
+            string opis = "";
+            if(ocena >=1 && ocena <=6)
+            {
+                switch(ocena)
+                {
+                    case 1:
+                        opis = "Niedostateczny";
+                        break;
+                    case 2:
+                        opis = "Dopuszczający";
+                        break;
+                    case 3:
+                        opis = "Dostateczny";
+                        break;
+                    case 4:
+                        opis = "Dobry";
+                        break;
+                    case 5:
+                        opis = "Bardzo dobry";
+                        break;
+                    case 6:
+                        opis = "Celujący";
+                        break;
+                }
+                Console.WriteLine($"Ocena {ocena} to {opis}");
+            }
+            else
+            {
+                Console.WriteLine("Błąd: ocena musi być w zakresie 1-6");
+            }   
         }
     }
 }
