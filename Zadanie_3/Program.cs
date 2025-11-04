361e220a78013054d2ed791bb378675111b326dd
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-11-04 12:31:51 +0100

    zadanie 3

---
+++ Zadanie_3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Zadanie_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperaturaCelsjusz = 25;
+            double temperaturaFahrenheit = Math.Round((temperaturaCelsjusz * 9 / 5) + 32,2);
+            double C = Math.Round((temperaturaFahrenheit - 32) * 5 / 9,2);
+            if (C < 0) { 
+                Console.WriteLine("Uwaga temperatura spada poniżej zera!");
+            }
+            Console.WriteLine($"Temperatura w Fahrenheitach: {temperaturaFahrenheit}");
+            Console.WriteLine($"Temperatura w Celsjuszach: {C}");
+        }
+    }
+}
