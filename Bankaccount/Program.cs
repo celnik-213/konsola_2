ca1024966e7107878cd669808615dcba6ac3a6ca
celnik-213 <uT_4000@zsp3zamosc.pl>
2025-11-06 14:34:55 +0100

    konto bankowe beta

---
+++ Bankaccount/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Bankaccount
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
